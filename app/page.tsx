bd9b694ccc49d6ec89e286db02701bdc67101aaf
Suraj <suraj.ukumar.p@gmail.com>
2024-07-28 20:34:17 +0530

    The project is initialized.

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-3xl font-bold text-indigo-500">
+      <h1>DevsDiscord</h1>
+    </div>
+  );
+}
